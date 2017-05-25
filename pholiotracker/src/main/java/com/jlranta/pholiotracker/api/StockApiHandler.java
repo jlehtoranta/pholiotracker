4b2d1e831f477812fb7e82883517e3a040e01550
Jarkko Lehtoranta <devel@jlranta.com>
2017-05-25 23:52:47 +0300

    [api] StockApiHandler: Initial commit

---
+++ pholiotracker/src/main/java/com/jlranta/pholiotracker/api/StockApiHandler.java
@@ -0,0 +1,34 @@
+package com.jlranta.pholiotracker.api;
+
+import java.util.ArrayList;
+import java.util.TreeMap;
+
+public class StockApiHandler {
+    private ArrayList<StockApi> stockApis;
+    
+    public StockApiHandler() {
+        this.initializeApis();
+    }
+    
+    public TreeMap<StockApi, ArrayList<String>> searchAll(String s) {
+        TreeMap<StockApi, ArrayList<String>> foundFromApis = new TreeMap<>();
+        
+        for(StockApi api : this.stockApis) {
+            ArrayList<String> found = api.search(s);
+            if (!found.isEmpty()) {
+                foundFromApis.put(api, found);
+            }
+        }
+        
+        return foundFromApis;
+    }
+    
+    public ArrayList<StockApi> getApis() {
+        return this.stockApis;
+    }
+    
+    private void initializeApis() {
+        StockApi vantage = new AlphaVantageApi();
+        this.stockApis.add(vantage);
+    }
+}
