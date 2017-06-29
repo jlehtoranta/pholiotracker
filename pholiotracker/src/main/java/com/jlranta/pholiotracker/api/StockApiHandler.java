48c5f038920df73c30fa8add3ae5a7ad71f10781
Jarkko Lehtoranta <devel@jlranta.com>
2017-06-29 21:50:25 +0300

    [api] Implement search functionalities in StockApiHandler + clean-up

--- pholiotracker/src/main/java/com/jlranta/pholiotracker/api/StockApiHandler.java
+++ pholiotracker/src/main/java/com/jlranta/pholiotracker/api/StockApiHandler.java
@@ -1,36 +1,65 @@
 package com.jlranta.pholiotracker.api;
 
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 
 /**
- * Initializes and handles all the APIs that implement the StockApi interface
- * @author Jarkko Lehtoranta <devel@jlranta.com>
+ * Initializes and handles all the APIs that implement the StockApi interface.
+ * @author Jarkko Lehtoranta
  */
 public class StockApiHandler {
     private ArrayList<StockApi> stockApis = new ArrayList<>();
+    private StockSearchApi stockSearch;
     
+    /**
+     * Creates a new StockApiHandler instance.
+     */
     public StockApiHandler() {
         this.initializeApis();
     }
     
     /**
-     * 
+     * Search for a stock.
+     * @param s A part of a stock name or a stock symbol
+     * @return  A list of StockSearchResults
+     */
+    public ArrayList<StockSearchResult> searchStock(String s) {
+        return this.stockSearch.search(s);
+    }
+    
+    /**
+     * Get the most recent price quote for a stock.
+     * @param s    A stock symbol
+     * @param exch A stock exchange, where the symbol is listed
+     * @return     The most recent price as a StockSearchResult
+     */
+    public StockSearchResult getCurrentPrice(String s, String exch) {
+        StockSearchResult res = null;
+        
+        for (StockApi api : this.stockApis) {
+            if (api.toString().equals("Alpha Vantage")) {
+                res = api.getQuote(s, exch);
+            }
+        }
+        
+        return res;
+    }
+    
+    /**
+     * Search for a stock API, which provides data for a stock symbol.
      * @param s A company name or a stock symbol
      * @return  A LinkedHashMap of StockApis as keys and a list of found
      *          stock symbols as strings
      */
-    public LinkedHashMap<StockApi, ArrayList<String>> searchAll(String s) {
-        LinkedHashMap<StockApi, ArrayList<String>> foundFromApis = new LinkedHashMap<>();
-        
+    public StockApi searchApi(String s) {
+
         for (StockApi api : this.stockApis) {
             ArrayList<String> found = api.search(s);
             if (!found.isEmpty()) {
-                foundFromApis.put(api, found);
+                return api;
             }
         }
         
-        return foundFromApis;
+        return null;
     }
     
     public ArrayList<StockApi> getApis() {
@@ -38,6 +67,8 @@ public class StockApiHandler {
     }
     
     private void initializeApis() {
+        this.stockSearch = new StockSearchApi();
+        
         StockApi vantage = new AlphaVantageApi();
         this.stockApis.add(vantage);
     }
