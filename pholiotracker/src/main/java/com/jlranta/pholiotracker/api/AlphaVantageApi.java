4b8b0cb025efcf6d0ff08edefe4e2908c1733a2a
Jarkko Lehtoranta <devel@jlranta.com>
2017-06-29 21:47:44 +0300

    [api] Implement getQuote + update getData + clean-up in AlphaVantageApi

--- pholiotracker/src/main/java/com/jlranta/pholiotracker/api/AlphaVantageApi.java
+++ pholiotracker/src/main/java/com/jlranta/pholiotracker/api/AlphaVantageApi.java
@@ -2,6 +2,12 @@ package com.jlranta.pholiotracker.api;
 
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.Date;
+import java.util.TimeZone;
+import java.util.Calendar;
+import java.text.ParseException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.io.IOException;
 import java.io.StringReader;
 import com.google.gson.stream.JsonReader;
@@ -9,7 +15,7 @@ import com.google.gson.stream.JsonReader;
 /**
  * This class implements the Alpha Vantage stock API. Have a look at the
  * in depth documentation of the methods in the StockApi interface.
- * @author Jarkko Lehtoranta <devel@jlranta.com>
+ * @author Jarkko Lehtoranta
  */
 public class AlphaVantageApi implements StockApi {
     private final String apiName = "Alpha Vantage";
@@ -17,6 +23,9 @@ public class AlphaVantageApi implements StockApi {
     private final String baseUrl = "http://www.alphavantage.co/query?";
     private RestApiQuery restQuery;
     
+    /**
+     * Creates a new API instance.
+     */
     public AlphaVantageApi() {
         this.restQuery = new RestApiQuery(this.apiName);
     }
@@ -28,25 +37,95 @@ public class AlphaVantageApi implements StockApi {
     
     @Override
     public ArrayList<String> search(String s) {
+        ArrayList<String> r = new ArrayList<>();
         
-        return new ArrayList<>();
+        r.add("");
+        
+        return r;
     }
     
     @Override
-    public LinkedHashMap<String, Double> getData(String s) {
-        LinkedHashMap<String, Double> data = new LinkedHashMap<>();
+    public StockSearchResult getQuote(String s, String exch) {
+        Double price = 0.0;
+        Date time = null;
         
-        String response = this.restQuery.apiRequest(this.baseUrl + "&apikey=" + this.apiKey + "&function=TIME_SERIES_INTRADAY&interval=1min&symbol=" + s);
-        System.out.println(response);
+        String response = this.restQuery.apiRequest(this.baseUrl + "&apikey=" + this.apiKey + "&function=GLOBAL_QUOTE&symbol=" + exch + ":" + s);
+        if (response.isEmpty()) {
+            return null;
+        }
         JsonReader parser = new JsonReader(new StringReader(response));
+        DateFormat df = new SimpleDateFormat("MMM dd, hh:mma z yyyy");
         try {
             parser.beginObject();
             while (parser.hasNext()) {
-                String name = parser.nextName();
-                if (name.contains("Time Series")) {
+                if (parser.nextName().equals("Realtime Global Securities Quote")) {
                     parser.beginObject();
                     while (parser.hasNext()) {
-                        String time = parser.nextName();
+                        switch (parser.nextName()) {
+                            case "03. Latest Price":
+                                price = parser.nextDouble();
+                                break;
+                            case "11. Last Updated":
+                                try {
+                                    time = df.parse(parser.nextString() + " " + Calendar.getInstance().get(Calendar.YEAR));
+                                } catch (ParseException e) {
+                                    System.err.println(this.apiName + " JSON date parsing failed" + e.toString());
+                                    parser.skipValue();
+                                }
+                                break;
+                            default:
+                                parser.skipValue();
+                                break;
+                        }
+                    }
+                    parser.endObject();
+                } else {
+                    parser.skipValue();
+                }
+            }
+            parser.endObject();
+        } catch (IOException e) {
+            System.err.println(this.apiName + " JSON parsing failed" + e.toString());
+        }
+        
+        return new StockSearchResult("", s, exch, price, time);
+    }
+    
+    @Override
+    public LinkedHashMap<Date, Double> getData(String s) {
+        LinkedHashMap<Date, Double> data = new LinkedHashMap<>();
+        
+        String response = this.restQuery.apiRequest(this.baseUrl + "&apikey=" + this.apiKey + "&function=TIME_SERIES_DAILY&symbol=" + s);
+        if (response.isEmpty()) {
+            return data;
+        }
+        JsonReader parser = new JsonReader(new StringReader(response));
+        try {
+            parser.beginObject();
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+            while (parser.hasNext()) {
+                String name = parser.nextName();
+                if (name.contains("Meta Data")) {
+                    parser.beginObject();
+                    while (parser.hasNext()) {
+                        if (parser.nextName().equals("6. Time Zone")) {
+                            df.setTimeZone(TimeZone.getTimeZone(parser.nextString()));
+                        } else {
+                            parser.skipValue();
+                        }
+                    }
+                    parser.endObject();
+                } else if (name.contains("Time Series")) {
+                    parser.beginObject();
+                    while (parser.hasNext()) {
+                        Date time;
+                        try {
+                            time = df.parse(parser.nextName());
+                        } catch (ParseException e) {
+                            System.err.println(this.apiName + " JSON date parsing failed" + e.toString());
+                            parser.skipValue();
+                            continue;
+                        }
                         Double price = 0.0;
                         parser.beginObject();
                         while (parser.hasNext()) {
