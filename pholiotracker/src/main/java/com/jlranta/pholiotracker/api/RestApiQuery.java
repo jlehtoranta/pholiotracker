76fb4c8572c3fb7f3bc5fd81a69ecea9c80030d8
Jarkko Lehtoranta <devel@jlranta.com>
2017-06-29 21:48:53 +0300

    [api] Update documentation

--- pholiotracker/src/main/java/com/jlranta/pholiotracker/api/RestApiQuery.java
+++ pholiotracker/src/main/java/com/jlranta/pholiotracker/api/RestApiQuery.java
@@ -12,13 +12,14 @@ import org.apache.http.util.EntityUtils;
 /**
  * RestApiQuery class is a helper class for placing HTTP queries to the stock
  * APIs. It uses org.apache.http.* for querying.
- * @author Jarkko Lehtoranta <devel@jlranta.com>
+ * @author Jarkko Lehtoranta
  */
 public class RestApiQuery {
     private String apiName = "";
     private CloseableHttpClient cli;
     
     /**
+     * Creates a new RestApiQuery instance.
      * @param apiName The display name of the API, where the queries come from
      */
     public RestApiQuery(String apiName) {
@@ -27,7 +28,7 @@ public class RestApiQuery {
     }
     
     /**
-     * A simple methods that handles the generic HTTP GET requests
+     * A simple methods that handles the generic HTTP GET requests.
      * @param url The URL for the HTTP GET request
      * @return The body of the HTTP response as a string
      */
